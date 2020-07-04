75e3cae75cc0f7195f7e5ae618dd73ed5436502d
aryam-024 <66679403+aryam-024@users.noreply.github.com>
2020-07-04 07:27:54 -0700

    done

---
+++ web-speedtest-6/Development/js/scripts.js
@@ -0,0 +1,29 @@
+// قم بإنشاء الدوال التالية
+
+/*
+ * greet(name) اسم الدالة
+ * - name قد تستقبل وقد لا تستقبل
+ * - name إن لم تستقبل اسم أو "Hello" تطبع
+ * - name إن استقبلت اسم أو "Hello <name>" تطبع
+ */
+
+
+function greet(name) {
+    if(name){
+    let message ="hello ";
+    message=message+ name;
+    console.loge(message);
+}
+else {console.log(message);
+
+}
+}
+greet();
+greet(hello);
+
+/*
+ * isOdd(n) اسم الدالة
+ * - n تستقبل رقم
+ * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
+ */
+function isOdd(n) {}
